def3ae8818bd5a5f36d3f4aaed2d03cee426ffef
sushanksinha <sushanksinha.5252@gmail.com>
2025-09-07 23:16:09 +0530

    update path of dbConnection file in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const cors = require("cors")
-const { dbConnection } = require("./db/config");
+const { dbConnection } = require("./config/db.config");
 const userRouter = require("./routers/user.routes")
 
 const app = express();
